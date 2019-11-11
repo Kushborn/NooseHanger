fc7520e8ca94b3c61b0fbf59fea714aa2f184fed
Benjamin <benfesterh@gmail.com>
2019-11-11 14:25:54 +0100

    Tilføjet recyclerview, dette giver lidt problemer. I recyclerviewet er der blandt andet circleimageview

--- app/src/main/java/com/example/noosehanger/HighscoreAktivitet.java
+++ app/src/main/java/com/example/noosehanger/HighscoreAktivitet.java
@@ -1,14 +1,67 @@
 package com.example.noosehanger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class HighscoreAktivitet extends AppCompatActivity {
+
+    private static final String TAG = "HighScoreAktivitet";
+    private ArrayList<String> mUserNames = new ArrayList<>();
+    private ArrayList<String> mScores = new ArrayList<>();
+    private ArrayList<Integer> mImageUrls = new ArrayList<>();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscore_aktivitet);
+
+        Log.d(TAG, "InitImageBitMaps: laver bitmaps.");
+
+        initImageBitamsp();
+
+
     }
+
+    private void initImageBitamsp() {
+
+        mImageUrls.add(R.drawable.reaper);
+        mUserNames.add("Benjamin");
+        mScores.add("9000");
+
+        mImageUrls.add(R.drawable.elephant);
+        mUserNames.add("Alexandra");
+        mScores.add("8999");
+
+        mImageUrls.add(R.drawable.doge);
+        mUserNames.add("Douglas");
+        mScores.add("6000");
+
+        mImageUrls.add(R.drawable.unicorn);
+        mUserNames.add("Birte");
+        mScores.add("666");
+
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView() {
+
+        Log.d(TAG, "initRecyclerView: init recycler.");
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mUserNames, mImageUrls, mScores);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
--- app/src/main/java/com/example/noosehanger/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/noosehanger/RecyclerViewAdapter.java
@@ -1,19 +1,74 @@
 package com.example.noosehanger;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdapter {
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> userNames = new ArrayList<>();
+    private ArrayList<Integer> images = new ArrayList<>();
+    private ArrayList<String> scores = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> userNames, ArrayList<Integer> images, ArrayList<String> scores) {
+        this.userNames = userNames;
+        this.images = images;
+        this.scores = scores;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: calles.");
+        Glide.with(mContext).asBitmap().load(images.get(position)).into(holder.image);
+
+        holder.name.setText(userNames.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + userNames.get(position));
+
+                Toast.makeText(mContext, userNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userNames.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
 
+        CircleImageView image;
         TextView name, score;
         RelativeLayout parentLayout;
 
@@ -21,6 +76,7 @@ public class RecyclerViewAdapter {
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            image = itemView.findViewById(R.id.profile_image);
             name = itemView.findViewById(R.id.name);
             score = itemView.findViewById(R.id.score);
 
